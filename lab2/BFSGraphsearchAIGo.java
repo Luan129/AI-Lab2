aceddc640c5e353db42cbdbf1c2497880862fb2c
LAPTOP-DHDBO7FM\VAN LUAN <vanluan0903@gmail.com>
2023-10-07 22:46:31 +0700

    first commit

---
+++ lab2/BFSGraphsearchAIGo.java
@@ -0,0 +1,73 @@
+package lab2;
+
+import java.awt.List;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class BFSGraphsearchAIGo implements ISearchAlgo { // ko trung
+
+	@Override
+	public Node execute(Node root, String goal) {
+		Queue<Node> frontier = new LinkedList<Node>();
+		frontier.add(root);
+		ArrayList<Node> ex = new ArrayList<Node>();
+		while (!frontier.isEmpty()) {
+			Node curent = frontier.poll();
+			if (curent.getLabel().equals(goal)) {
+				return curent;
+
+			} else {
+				ex.add(curent);
+				java.util.List<Node> children = curent.getChildrenNodes();
+				for (Node child : children) {
+					if (!frontier.contains(child) && (!ex.contains(child))) {
+						frontier.add(child);
+						child.setParent(curent);
+
+					}
+				}
+			}
+
+		}
+		return null;
+	}
+
+	@Override
+	public Node execute(Node root, String start, String goal) {
+		Node n = execute(root, start);
+		n.setParent(null);
+		Node node = execute(n, goal);
+		return node;
+	}
+
+	public static void main(String[] args) {
+		BFSGraphsearchAIGo go = new BFSGraphsearchAIGo();
+		Node nS = new Node("S");
+		Node nA = new Node("A");
+		Node nB = new Node("B");
+		Node nC = new Node("C");
+		Node nD = new Node("D");
+		Node nE = new Node("E");
+		Node nF = new Node("F");
+		Node nG = new Node("G");
+		Node nH = new Node("H");
+		nS.addEdge(nA, 5);
+		nS.addEdge(nB, 2);
+		nS.addEdge(nC, 4);
+		nA.addEdge(nD, 9);
+		nA.addEdge(nE, 4);
+		nB.addEdge(nG, 6);
+		nC.addEdge(nF, 2);
+		nD.addEdge(nH, 7);
+		nE.addEdge(nG, 6);
+		nF.addEdge(nG, 1);
+		Node node = go.execute(nS, "G");
+		NodeUtils n1 = new NodeUtils();
+		System.out.println(n1.printPath(node));
+		Node node1 = go.execute(nS, "C", "G");
+		System.out.println(n1.printPath(node1));
+
+	}
+
+}
---
+++ lab2/DFSTreeSearchAIGo.java
@@ -0,0 +1,71 @@
+package lab2;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class DFSTreeSearchAIGo implements ISearchAlgo {
+
+	@Override
+	public Node execute(Node root, String goal) {
+		// TODO Auto-generated method stub
+		Stack<Node> frotiner = new Stack<>();
+		frotiner.push(root);
+		while (!frotiner.isEmpty()) {
+			Node current = frotiner.pop();
+			if (current.getLabel().equals(goal)) {
+				return current;
+			} else {
+				List<Node> children = current.getChildrenNodes();
+				for (int i = children.size() - 1; i >= 0; i--) {
+					Node child = children.get(i);
+					if (!frotiner.contains(child)) {
+						frotiner.push(child);
+						child.setParent(current);
+						System.out.println(child.getLabel());
+						System.out.println(child.getParent().getLabel());
+
+					}
+				}
+			}
+		}
+		return null;
+	}
+
+	@Override
+	public Node execute(Node root, String start, String goal) {
+		// TODO Auto-generated method stub
+		Node n = execute(root, start);
+		n.setParent(null);
+		Node node = execute(n, goal);
+		return node;
+	}
+
+	public static void main(String[] args) {
+		DFSGraphSearchAIGo go = new DFSGraphSearchAIGo();
+		Node nS = new Node("S");
+		Node nA = new Node("A");
+		Node nB = new Node("B");
+		Node nC = new Node("C");
+		Node nD = new Node("D");
+		Node nE = new Node("E");
+		Node nF = new Node("F");
+		Node nG = new Node("G");
+		Node nH = new Node("H");
+		nS.addEdge(nA, 5);
+		nS.addEdge(nB, 2);
+		nS.addEdge(nC, 4);
+		nA.addEdge(nD, 9);
+		nA.addEdge(nE, 4);
+		nB.addEdge(nG, 6);
+		nC.addEdge(nF, 2);
+		nD.addEdge(nH, 7);
+		nE.addEdge(nG, 6);
+		nF.addEdge(nG, 1);
+		Node node = go.execute(nS, "G");
+		NodeUtils n = new NodeUtils();
+		System.out.println(n.printPath(node));
+		Node node2 = go.execute(nS, "A", "G");
+		System.out.println(n.printPath(node2));
+	}
+}
